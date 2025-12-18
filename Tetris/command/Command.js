3d3bf4597edfc17e9abfba6ef442f4604c6c1214
stomach-stop1106 <chha23069@g.nihon-u.ac.jp>
2025-12-19 02:53:17 +0900

    Delete Tetris directory

--- Tetris/command/Command.js
+++
@@ -1,9 +0,0 @@
-class Command{ //Commandパターン
-    constructor(){
-        this.game = this.game;
-    }
-
-    excute(){ //操作を実行
-        throw new Error("Execute method must be implemented.");
-    }
-}
