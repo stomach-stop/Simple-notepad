1e763b0ea16d24fc9cd5017dbf734a4f6337efff
stomach-stop <166628050+stomach-stop@users.noreply.github.com>
2025-12-23 02:47:42 +0900

    Delete Tetris directory

--- Tetris/core/GameSetting.js
+++
@@ -1,9 +0,0 @@
-class GameSetting{ //変更可能な設定を管理
-    #blockSize;
-    get blockSize() { return this.#blockSize }
-    set blockSize(newValue) { this.#blockSize = newValue }
-
-    #dropInterval = 1000;
-    get dropInterval() { return this.#dropInterval }
-    set dropInterval(newValue) { this.#dropInterval = newValue }
-}
